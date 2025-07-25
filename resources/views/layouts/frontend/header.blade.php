2e2250da17612b38834095cc7a89ad13e1d5e2c6
Uzair mirza <UZAIRMIRZA2121@GMAIL.COM>
2025-07-25 16:04:14 +0500

    Ask Law text

--- resources/views/layouts/frontend/header.blade.php
+++ resources/views/layouts/frontend/header.blade.php
@@ -2,16 +2,18 @@
 <header class="site-header">
     <div class="container header-container">
         <div class="logo">
-            <img src="logo.png" alt="Lawyers of Pakistan Logo" />
+            <a href="{{ route('home') }}" class="text-white text-decoration-none">
+                <h3 class="mb-0">Ask Law</h3>
+            </a>
         </div>
         <nav class="nav">
             <ul>
-                <li><a href="{{ route('home')}}">Home</a></li>
-                <li><a href="{{ route('services')}}">Legal Services</a></li>
-                <li><a href="{{ route('team')}}">Team</a></li>
+                <li><a href="{{ route('home') }}">Home</a></li>
+                <li><a href="{{ route('services') }}">Legal Services</a></li>
+                <li><a href="{{ route('team') }}">Team</a></li>
                 {{-- <li><a href="#">Foreigners</a></li> --}}
-                <li><a href="{{ route('blogs')}}">Blog</a></li>
-                <li><a href="{{ route('contact')}}">Contact Us</a></li>
+                <li><a href="{{ route('blogs') }}">Blog</a></li>
+                <li><a href="{{ route('contact') }}">Contact Us</a></li>
                 @guest
                     @if (Route::has('login'))
                         <li><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
