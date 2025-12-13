dad9cea69b9835e36cce8f0e51e9fa55e7d74309
Uzair mirza <UZAIRMIRZA2121@GMAIL.COM>
2025-12-13 12:42:44 +0500

    done

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -217,7 +217,7 @@ class ClientController extends Controller
             $client->assignedUsers()->detach();
         }
 
-        return redirect()->back()->with('success', 'Client updated successfully.');
+        return redirect()->route('clients.show')->with('success', 'Client updated successfully.');
     }
 
 
