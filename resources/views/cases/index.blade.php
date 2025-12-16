a9448e393b576a6be2772480867c0aed5aa5742f
Uzair mirza <UZAIRMIRZA2121@GMAIL.COM>
2025-12-16 19:35:08 +0500

    Refactor script inclusion to use stack for better organization and maintainability

--- resources/views/cases/index.blade.php
+++ resources/views/cases/index.blade.php
@@ -380,6 +380,8 @@
             window.print();
         }
     </script>
-
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+@endpush
 
 @endsection
--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -44,6 +44,8 @@
     </a>
 
     @include('layouts.frontend.footer')
+    <!-- Stack for page-specific scripts -->
+@stack('scripts')
     <script src="{{ asset('public/asset/js/script.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
